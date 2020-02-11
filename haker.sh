f9e0297199862cb3a0a01280a8b3e4af91fab64a
Igor Ordecha <35789870+igoro00@users.noreply.github.com>
2020-02-11 10:09:46 +0100

    Update haker.sh

--- haker.sh
+++ haker.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/igoro00/cmatrix /tmp/cmatrix/
-sudo apt install cmake libncurses5-dev -y
+sudo apt install git cmake libncurses5-dev -y
 mkdir -p /tmp/cmatrix/build
 cd /tmp/cmatrix/build
 cmake ..
