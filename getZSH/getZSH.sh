3cce25314a43dfc15de852194072be14cf02298b
Igor Ordecha <35789870+igoro00@users.noreply.github.com>
2020-03-16 00:49:39 +0100

    Update getZSH.sh

--- getZSH/getZSH.sh
+++ getZSH/getZSH.sh
@@ -1,6 +1,7 @@
-sudo apt -y install zsh wget git
-sh -c "$(wget -O- https://raw.githubusercontent.com/igoro00/Scripts/master/getZSH/install_ohmyzsh.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-cat "$(wget -O- https://raw.githubusercontent.com/igoro00/Scripts/master/getZSH/zshrc)" > ~/.zshrc
-cat "$(wget -O- https://raw.githubusercontent.com/igoro00/Scripts/master/getZSH/p10k.zsh)" > ~/.p10k.zsh
-exec zsh -l
+sudo apt -y install zsh wget git sed
+sh -c "$(wget -O- https://raw.githubusercontent.com/igoro00/Scripts/master/getZSH/ohmyzsh.sh)"
+wget  https://raw.githubusercontent.com/igoro00/Scripts/master/getZSH/zshrc -O ~/.zshrc
+sed -i -e 's|~|'$HOME'|g' ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/themes/powerlevel10k
+wget  https://raw.githubusercontent.com/igoro00/Scripts/master/getZSH/p10k.zsh -O ~/.p10k.zsh
+zsh
