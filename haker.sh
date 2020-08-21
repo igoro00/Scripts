5ca57c34f5899070a594d4ff1faf2bd6c3e3b031
Igor Ordecha <35789870+igoro00@users.noreply.github.com>
2020-08-21 02:45:18 +0200

    Delete haker.sh

--- haker.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install git cmake libncurses5-dev -y
-git clone https://github.com/igoro00/cmatrix /tmp/cmatrix/
-mkdir -p /tmp/cmatrix/build
-cd /tmp/cmatrix/build
-cmake ..
-make
-mv cmatrix ~/cmatrix
-~/cmatrix -M "Hacking in Progress" -L
